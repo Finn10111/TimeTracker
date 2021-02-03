1417a57477e759058a57c4431f40abd28ddb5c4a
Finn Christiansen <kontakt@finnchristiansen.de>
2021-02-03 21:46:24 +0100

    Catch errors and show a message

--- frontend/app/components/timeperiod-list.js
+++ frontend/app/components/timeperiod-list.js
@@ -1,10 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class TimeperiodListComponent extends Component {
+  @service flashMessages;
+
   @action
   delete(timeperiod) {
+    var self = this;
     timeperiod.destroyRecord().then(function() {
+    })
+    .catch(function(){
+      self.flashMessages.danger('Something went wrong!');
     });
   }
 }
